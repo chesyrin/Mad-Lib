fd0012b4c06f4236962bb63b867997d1562c5ddb
chesyrin <chesyrin@users.noreply.github.com>
2018-07-17 22:35:58 -0400

    Add files via upload

---
+++ madLib.py
@@ -0,0 +1,11 @@
+# Project #3: Mad Libs Generator
+# Prompts user for singular noun, adjective etc. and creates story
+# July 8 2018
+
+colour = input ("Please enter colour: ")
+item = input ("Please enter an item: ")
+action = input ("Please enter an action in past tense: ")
+
+print ("One day, a(n) "  + colour + ''' alien was walking down the street.
+All of a sudden, he saw a flying ''' + item + ". Immediately, he " +
+       action + " and then ran away.")
